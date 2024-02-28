4ed2ba5c710c54a614772524839e0ed0aa892bf1
drlappies <yau1chun2lap3@gmail.com>
2024-02-28 21:51:36 +0800

    add assets

---
+++ src/Asset.ts
@@ -0,0 +1,13 @@
+const Asset = {
+  bg1: process.env.PUBLIC_URL + "/assets/bg1.png",
+  bg2: process.env.PUBLIC_URL + "/assets/bg2.png",
+  boat: process.env.PUBLIC_URL + "/assets/boat.png",
+  e1: process.env.PUBLIC_URL + "/assets/e1.png",
+  e2: process.env.PUBLIC_URL + "/assets/e2.png",
+  p1: process.env.PUBLIC_URL + "/assets/p1.png",
+  p2: process.env.PUBLIC_URL + "/assets/p2.png",
+  p3: process.env.PUBLIC_URL + "/assets/p3.png",
+  p4: process.env.PUBLIC_URL + "/assets/p4.png",
+};
+
+export default Asset;
